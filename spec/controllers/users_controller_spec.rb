735ad399760fd72d638b977a09258ebaea682454
rkc2134 Rachel Chung <rkc2134@columbia.edu>
2022-11-16 01:15:17 +0000

    clean up some code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,16 +6,13 @@ RSpec.describe UsersController, type: :controller do
     if User.where(:username => 'anna123').empty?
         User.create(:username => 'anna123', :email => 'anna123@columbia.edu', :password => 'anna123')
     end
-    if Event.where(:title => 'WiCS Tabling').empty?
-        Event.create(:title => 'WiCS Tabling', :freebie => 'FOOD', :event_date => '2022-09-07', :event_time => '10:00-11:00AM', :affiliation => 'WiCS')
-    end
   end
 
   describe "create new user" do
     it "should create a new" do
       get :create, {:user => {:id => 20, :username => 'anna123', :email => 'anna123@columbia.edu', :password => 'anna123'}}
       expect(response).to redirect_to events_path
-      User.find_by_id(20).destroy
+      User.find_by(username: 'anna123').destroy
     end
   end
 
