8bf4f916ffd74e23837ee19820bf33b2e63f9516
rkc2134 Rachel Chung <rkc2134@columbia.edu>
2022-12-16 00:29:28 +0000

    added rspec tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,4 +9,29 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
+  describe "login with invalid email/password" do
+    it "gives error message" do
+      post :create, session: {:email => 'anna123@columbia.edu', :password => 'anna123'}
+      expect(flash[:warning]).to match('Invalid email/password combination')
+    end
+  end
+
+  # describe "login with valid and confirmed email/password" do
+  #   it "successfully logs in" do
+  #     User.create(:id => 20, :username => 'anna123', :email => 'anna123@columbia.edu', :password => 'anna123', :email_confirmed => true)
+  #     post :create, session: {:email => 'anna123@columbia.edu', :password => 'anna123'}
+  #     expect(response).to redirect_to events_path
+  #     User.find_by_username('anna123').destroy
+  #   end
+  # end
+
+  # describe "login without confirmed email" do
+  #   it "gives error message" do
+  #     User.create(:id => 20, :username => 'anna123', :email => 'anna123@columbia.edu', :password => 'anna123')
+  #     post :create, session: {:email => 'anna123@columbia.edu', :password => 'anna123'}
+  #     expect(flash[:warning]).to match('Please activate your account by following the instructions in the account confirmation email you received to proceed')
+  #     User.find_by_username('anna123').destroy
+  #   end
+  # end
+
 end
